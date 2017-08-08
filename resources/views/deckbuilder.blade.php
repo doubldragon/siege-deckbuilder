8589cc85b3618481ff2f0838c13bfa0dd99ab2b4
Brandon Spencer <brandon.spencer@gmail.com>
2017-08-08 15:22:21 -0400

    Fixing css display issues

--- resources/views/deckbuilder.blade.php
+++ resources/views/deckbuilder.blade.php
@@ -5,47 +5,46 @@
 
 	<div class='container text-center' ng-app="app" ng-controller="Controller">
 		<h1 class="banner">Siege! Deckbuilder</h1>
-		<div ng-hide="selectLead">
-			<div ng-hide="newExisting" class="deckHeader col-md-6 center-block" >
-			<button class="btn btn-primary mt-3" ng-click="newExisting=true;">Create New Deck</button>
-			<br /><br />
-			<p class="mb-1">or Select Existing Deck to Edit</p>
-			
-			<form class='form-inline'>
-				<div class="form-group" style='margin: 0 auto;'>
-			    	
+		
+<div ng-hide="selectLead" class="">
+	<div ng-hide="newExisting" >
+		<button class="btn btn-success mt-3" ng-click="newExisting=true;">Create New Deck</button>
+		<p class="mb-1">or Select Existing Deck to Edit</p>
+		<form class='form-inline text-center'>
+			<div class="form-group" style='margin: 0 auto;'>
+		    	
 			    <select class="form-control mb-4" id="deckSelect" name="deckSelect">
 			      <option name='' value=''>Choose Deck</option>
 			      <option name='deckSelect' ng-repeat="deck in decks" value="[[deck]]">[[deck.name]]</option>
 			    </select>
-					<button style="margin: 0 auto;" class='btn btn-primary mb-4' type="submit" ng-click='openDeck()'>Select Deck</button>
-				</div>
-			</form>
+				<button style="margin: 0 auto;" class='btn btn-success mb-4' type="submit" ng-click='openDeck()'>Select Deck</button>
 			</div>
-			<!-- <form class='form-inline' > -->
-				<div ng-show="newExisting" class='form-group' style='margin: 0 auto;'>	
-					
-				<button class="btn btn-primary mr-1 ml-1 mb-4" ng-click="isMonarch=true; selectFaction=true;">Monarch</button>
-				<button class="btn btn-primary mr-1 ml-1 mb-4" ng-click="isMonarch=false; selectFaction=true;">Invader</button>
-				<!-- <hr> --><br />
-				
-				<div class="col-md-4 center-block" ng-show="selectFaction"   ng-repeat="card in cards | filter: {isMonarch: isMonarch} | filter: {type_id: 1}" style="display:inline-block; ">
-				<div class="panel panel-default deckHeader" style="height:200px;">
+		</form>
+	</div>
+		<div ng-show="newExisting" class='form-group'>	
+				<div class="container" >
+				<button class="btn btn-success mr-1 ml-1 mb-4 mt-2" ng-click="isMonarch=true; selectFaction=true;">Monarch</button>
+				<button class="btn btn-success mr-1 ml-1 mb-4 mt-2" ng-click="isMonarch=false; selectFaction=true;">Invader</button>
+				</div><br />
+				<!-- <div class="col-md-2"></div> -->
+				<div   ng-show="selectFaction"  style="display: inline-block;" class="row">
+				<div class="col col-md-2"></div>
+				<div ng-repeat="card in cards | filter: {isMonarch: isMonarch} | filter: {type_id: 1}" class="ml-1 mr-1 col-md-4 panel panel-default " style="height:250px;">
 					<div class=" col-sm-12 panel-heading">  [[card.name]]     </div>
 					<div class="panel-body">
 						[[card.flavor_text]] <br />
-						<button ng-class="{'btn-primary': [[isMonarch]], 'btn-danger': ![[isMonarch]]}" class="btn mr-1 ml-1" ng-click="revealCards(card)">Select</button>
+						<button ng-class="{'btn-success': [[isMonarch]], 'btn-danger': ![[isMonarch]]}" class="btn mr-1 ml-1" ng-click="revealCards(card)">Select</button>
 
 					</div>
 				</div>
 				</div>
 			<!-- </form> -->
 			</div>
-		</div>
-		
+
+</div>		
 
 		<div ng-show="selectLead">
-			<button class="btn btn-primary mb-3" ng-click="resetForm()">Reset Builder</button>
+			<button class="btn btn-success mb-3" ng-click="resetForm()">Reset Builder</button>
 			<div class='row'>
 
 				<div class='col col-md-6' id='myDeck'>
@@ -155,7 +154,7 @@
 						<input type="text" name="name" ng-model="deckName"  placeholder="Name Your Deck">
 
 						
-						<button class='btn btn-primary' value="submit"  name='saveDeck' ng-disabled="deckPoints>maxPoints">Save Deck</button>
+						<button class='btn btn-success' value="submit"  name='saveDeck' ng-disabled="deckPoints>maxPoints">Save Deck</button>
 						<!-- <button class='btn btn-outline-danger' value='submit' name='deleteDeck'>Delete Deck</button> -->
 					</form>
 					<form method='post' action="[[editAction]]" ng-show="isEdit"> 
@@ -169,7 +168,7 @@
 						<input type="text" name="name" ng-model="deckName" placeholder="Name Your Deck">
 
 						
-						<button class='btn btn-primary' value="submit"  name='saveDeck' ng-disabled="deckPoints>maxPoints">Save Edit</button>
+						<button class='btn btn-success' value="submit"  name='saveDeck' ng-disabled="deckPoints>maxPoints">Save Edit</button>
 						<!-- <button class='btn btn-outline-danger' value='submit' name='deleteDeck'>Delete Deck</button> -->
 					</form>
 				</div>
