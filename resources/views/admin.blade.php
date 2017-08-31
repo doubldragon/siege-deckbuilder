32e36783ba9500fa7eb7f8580e8a8a3079a086bd
Brandon Spencer <brandon.spencer@gmail.com>
2017-08-31 14:33:09 -0400

    fixed merge error and auth issue

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,20 @@
+@extends ('layouts.app')
+
+
+@section('content')
+
+<div class="container" ng-app="app" ng-controller="Controller">
+	<ul class="nav nav-tabs">
+	  <li class="nav-item">
+	    <a class="nav-link active" href="#">Admin Tools</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link" href="#">Stats</a>
+	  </li>
+	</ul>
+	<h1>Admin tools</h1>
+</div>
+
+
+
+@endsection
