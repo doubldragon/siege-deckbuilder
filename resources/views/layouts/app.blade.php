afc2cf873276a3eb9e5953f2bc073ebeefd84c33
Brandon Spencer <brandon.spencer@gmail.com>
2017-08-31 13:40:55 -0400

    added admin blade and route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,9 @@
                         <li ><a href="/home">Home</a></li>
                         <li ><a href="/deckbuilder">Deckbuilder</a></li>
                         <li ><a href="/about">About</a></li>
+                        @if (Auth::user()->isAdmin)
+                            <li><a href="/admin">Admin</a></li>
+                        @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
